a7fac27120930f0aab6c74b7cb62d2d97caa59a4
Beyondcg <gcbeyond@sina.com>
2015-03-13 21:36:36 +0800

    text

---
+++ js/js.js
@@ -0,0 +1,7 @@
+$('#DB_gallery').DB_gallery({
+		thumWidth:110,            
+		thumGap:8,                
+		thumMoveStep:4,           
+		moveSpeed:300,            
+		fadeSpeed:500            
+	});
